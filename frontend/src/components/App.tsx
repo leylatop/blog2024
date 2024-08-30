5b586ddcbbcdf9460712f299719378fc352071d7
LeylaTop <775177772@qq.com>
2024-08-30 21:59:01 +0800

    init repo

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold">Hello, React with TypeScript and Tailwind CSS!</h1>
+    </div>
+  );
+};
+
+export default App;
